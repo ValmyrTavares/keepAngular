6c2f00a3d72455d10660e7481eeef5773730d7a8
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-12-08 12:55:11 -0300

    Esse estágio é referente a aula de Property Binding Interpolação, Style Binding, Event Bindng e Two way ddata binding

--- src/app/meu-segundo/meu-segundo.component.ts
+++ src/app/meu-segundo/meu-segundo.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'app-meu-segundo',
   templateUrl: './meu-segundo.component.html',
   styleUrls: ['./meu-segundo.component.css']
 })
-export class MeuSegundoComponent {}
+export class MeuSegundoComponent {
+nome:string = "abc"
+pessoa:any = {
+  nome:"Eu",
+  idade:10
+}
+
+}
